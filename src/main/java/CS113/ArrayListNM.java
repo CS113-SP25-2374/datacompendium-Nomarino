da5b76a31b4c88b13760f244eef4222d69a24ca1
Nomarino <Noahmarino316@gmail.com>
2025-02-18 20:30:08 -0800

    Finished Class Activity - Noah Marino

---
+++ src/main/java/CS113/ArrayListNM.java
@@ -0,0 +1,127 @@
+package CS113;
+
+public class ArrayListNM<E> implements ListInterface<E> {
+    private E[] array;
+    final static int DEFAULT_SIZE = 10;
+
+    public ArrayListNM() {
+        array = (E[]) new Object[DEFAULT_SIZE];
+    }
+
+    private void resize() {
+        E[] resizeArray = (E[]) new Object[array.length + (array.length / 2)];
+        for (int i = 0; i < array.length; i++) {
+            resizeArray[i] = array[i];
+        }
+        array = resizeArray;
+    }
+
+    @Override
+    public boolean add(E element) {
+        if (array[array.length - 1] != null) {
+            int length = array.length;
+            resize();
+            array[length] = element;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == null) {
+                array[i] = element;
+                return true;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void add(int index, E element) {
+        if (index < 0 || index >= array.length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (array[array.length - 1] != null) {
+            resize();
+        } else if (index == array.length - 1) {
+            array[array.length - 1] = element;
+        }
+
+        for (int i = size(); i >= index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = element;
+
+//        for (int i = index; i < array.length - 1; i++) {
+//            E temp = array[(i + 1)];
+//            array[(i + 1)] = array[i];
+//            array[i] = element;
+//            element = temp;
+//        }
+    }
+
+    @Override
+    public void clear() {
+        for (E element : array) {
+            element = null;
+        }
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return indexOf(element) >= 0;
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        if (object == null) {
+            return -1;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (object.equals(array[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= array.length) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    @Override
+    public boolean remove(int index) {
+        if (index < 0 || index >= array.length) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = null;
+        return true;
+    }
+
+    @Override
+    public boolean remove(E element) {
+        int index = indexOf(element);
+        return remove(index);
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void set(int index, E element) {
+
+    }
+}
